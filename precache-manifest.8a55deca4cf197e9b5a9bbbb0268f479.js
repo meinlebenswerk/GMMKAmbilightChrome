54e4a33c8c8eacae9870e2d995315a918916ab7e
Jan Eckert <jan.eckert@tum.de>
2021-04-09 22:55:12 +0200

    publish

---
+++ precache-manifest.8a55deca4cf197e9b5a9bbbb0268f479.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40e4efd2ca872e439bff",
+    "url": "/GMMKAmbilightChrome/css/app.5fa9a7f6.css"
+  },
+  {
+    "revision": "2604b751e0c3cefd3fdf0d08deafdd6a",
+    "url": "/GMMKAmbilightChrome/index.html"
+  },
+  {
+    "revision": "40e4efd2ca872e439bff",
+    "url": "/GMMKAmbilightChrome/js/app.4f651b8a.js"
+  },
+  {
+    "revision": "8169ca097761a0675f76",
+    "url": "/GMMKAmbilightChrome/js/chunk-vendors.5c38ec37.js"
+  },
+  {
+    "revision": "f01009eca61445fee2bdefa4426eed61",
+    "url": "/GMMKAmbilightChrome/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/GMMKAmbilightChrome/robots.txt"
+  }
+]);
